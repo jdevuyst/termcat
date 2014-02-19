d7e13441a176d3fa55a476f8f69099eb79eca8da
Jonas De Vuyst <jdv@foobar.be>
2014-02-19 17:04:16 +0800

    We now handle user-entered newlines correctly.

---
+++ doc/termcat-demo.js
@@ -0,0 +1,51 @@
+var forEach = Array.prototype.forEach;
+
+var inframe = document.getElementById('in');
+var outframe = document.getElementById('out');
+var inbody;
+
+var busy = false;
+var dirty = true;
+
+var worker = new Worker('termcat.js');
+
+function render() {
+  if(! busy && dirty) {
+    busy = true;
+    dirty = false;
+    worker.postMessage(inbody.textContent);
+}
+}
+
+worker.addEventListener('message', function (e) {
+  busy = false;
+  outframe.srcdoc = e.data;
+  setTimeout(render, 400);
+}, false);
+
+addEventListener('load', function () {
+  inbody = inframe.contentDocument.body;
+  inframe.contentDocument.designMode = 'on';
+  render();
+});
+
+inframe.contentWindow.addEventListener('keydown', function (e) {
+  if(e.keyCode == 13) {
+    var sel = inframe.contentWindow.getSelection();
+    var linebreak = inframe.contentDocument.createTextNode('\n');
+    sel.getRangeAt(0).insertNode(linebreak);
+    sel.removeAllRanges();
+    var newrange = inframe.contentDocument.createRange();
+    newrange.setStart(linebreak, 1);
+    newrange.setEnd(linebreak, 1);
+    sel.addRange(newrange);
+    e.preventDefault();
+    dirty = true;
+    setTimeout(render, 1);
+  }
+});
+
+inframe.contentWindow.addEventListener('input', function () {
+     dirty = true;
+     setTimeout(render, 400);
+});
