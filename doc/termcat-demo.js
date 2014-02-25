676c916e44e3d4ac6b1bf75823f1f7917cef1fc7
Jonas De Vuyst <jdv@foobar.be>
2014-02-26 00:44:08 +0700

    Now pasting plain text rather than HTML. Also made loading faster, and made loading not fail if MathJax is unavailable.

--- doc/termcat-demo.js
+++ doc/termcat-demo.js
@@ -1,11 +1,23 @@
 var inframe;
 var outframe;
+var inwin;
+var indoc;
 var inbody;
 
 var busy = false;
 var dirty = true;
 
 var reactapp;
+
+function escape(c) {
+  switch(c) {
+    case "<": return "&lt;"; break;
+    case ">": return "&gt;"; break;
+    case "&": return "&amp;"; break;
+    case "'": return "&apos;"; break;
+    case "\"": return "&quot;"; break;
+  }
+}
 
 function render() {
   if(! busy && dirty) {
@@ -15,9 +27,60 @@ function render() {
   }
 }
 
+function scrollToCaret() {
+  var el = indoc.createElement('span');
+  var sel = inwin.getSelection();
+
+  inwin.getSelection().getRangeAt(0).insertNode(el);
+
+  var minTop = el.offsetTop - inwin.innerHeight + el.offsetHeight;
+  if(inwin.scrollY < minTop) {
+    inwin.scroll(0, minTop);
+  }
+}
+
 addEventListener('DOMContentLoaded', function () {
   inframe = document.getElementById('in');
   outframe = document.getElementById('out');
+  inwin = inframe.contentWindow;
+
+  inwin.addEventListener('load', function () {
+    indoc = inframe.contentDocument;
+    inbody = indoc.body;
+    indoc.designMode = 'on';
+    inwin.focus();
+  });
+
+  inwin.addEventListener('keydown', function (e) {
+    if(e.keyCode == 13) {
+      e.preventDefault();
+
+      indoc.execCommand("insertHTML", false, "\n");
+
+      scrollToCaret();
+
+      dirty = true;
+      setTimeout(render, 1);
+    }
+  });
+
+  inwin.addEventListener('input', function () {
+       dirty = true;
+       setTimeout(render, 450);
+  });
+
+  inwin.addEventListener("paste", function(e) {
+    e.preventDefault();
+
+    var text = e.clipboardData.getData("text/plain");
+    text = text.replace(/[<>&'"]/g, escape)
+    indoc.execCommand("insertHTML", false, text);
+
+    scrollToCaret();
+
+    dirty = true;
+    setTimeout(render, 1);
+  });
 
   worker.addEventListener('message', function (e) {
     busy = false;
@@ -25,47 +88,9 @@ addEventListener('DOMContentLoaded', function () {
     setTimeout(render, 400);
   }, false);
 
-  inframe.contentWindow.addEventListener('keydown', function (e) {
-    if(e.keyCode == 13) {
-      var sel = inframe.contentWindow.getSelection();
-
-      sel.getRangeAt(0).deleteContents();
-
-      var linebreak = inframe.contentDocument.createElement("span");
-      linebreak.appendChild(inframe.contentDocument.createTextNode('\n'));
-      sel.getRangeAt(0).insertNode(linebreak);
-      sel.removeAllRanges();
-
-      var newrange = inframe.contentDocument.createRange();
-      newrange.setStartAfter(linebreak);
-      sel.addRange(newrange);
-
-      var minTop = linebreak.offsetTop - inframe.contentWindow.innerHeight + linebreak.offsetHeight * 2;
-      if(inframe.contentWindow.scrollY < minTop) {
-        inframe.contentWindow.scroll(0, minTop);
-      }
-
-      e.preventDefault();
-
-      dirty = true;
-      setTimeout(render, 1);
-    }
-  });
-
-  inframe.contentWindow.addEventListener('input', function () {
-       dirty = true;
-       setTimeout(render, 350);
-  });
-
   reactapp = React.createClass({
     render: function() {
       return React.DOM.html({dangerouslySetInnerHTML:{__html: this.props.doc}});
     }
   });
-});
-
-addEventListener('load', function () {
-  inbody = inframe.contentDocument.body;
-  inframe.contentDocument.designMode = 'on';
-  inframe.contentWindow.focus();
 });
